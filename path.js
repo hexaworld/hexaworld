5156163b6518b84a5fbb0baa4a010f8a66c28b99
freeman <the.freeman.lab@gmail.com>
2015-10-21 14:38:13 -0400

    Add paths

--- path.js
+++ path.js
@@ -4,19 +4,31 @@ _ = require('lodash')
 module.exports = Path
 
 function Path(options){
+  var options = options || {}
   this.position = 0
   this.type = 'path'
-  this.size = options.size || 50
-  this.color = options.color || '#DFE0E2s'
+  this.width = options.width || 0.2
+  this.color = options.color || '#939597'
 }
 
-Path.prototype.boundaries = function(position) {
-	// figure out rectangle given tile and position in pixel space
-	//var bbox = aabb([position.x, this.position.y], [this.size.x, this.size.y]);
+Path.prototype.border = function(origin) {
+  var self = this
+  var bottom = origin.y + origin.size * Math.sqrt(3)/2
+  return [
+    {x: origin.x - self.width/2 * origin.size, y: origin.y},
+    {x: origin.x - self.width/2 * origin.size, y: bottom},
+    {x: origin.x + self.width/2 * origin.size, y: bottom},
+    {x: origin.x + self.width/2 * origin.size, y: origin.y}
+  ]
 }
 
-Path.prototype.render = function(camera, context, position) {
-	console.log(position)
-	context.fillStyle = this.size
-  	context.fillRect(position.x, position.y, this.size, this.size)
+Path.prototype.render = function(context, origin) {
+  var border = this.border(origin)
+  context.beginPath()
+  _.forEach(border, function(point) {
+    context.lineTo(point.x, point.y)
+  })
+  context.closePath()
+  context.fillStyle = this.color
+  context.fill()
 }
