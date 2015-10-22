727daa768eaf03f7b13ef3d0ce658b6429b08c08
freeman <the.freeman.lab@gmail.com>
2015-10-21 21:37:36 -0400

    Switch zoom keys

--- camera.js
+++ camera.js
@@ -44,11 +44,11 @@ Camera.prototype.keyboardInput = function(keyboard){
     this.velocity.y = -this.speed;
   }
 
-  if (',' in keyboard.keysDown){
+  if ('.' in keyboard.keysDown){
     this.velocity.z = this.speed
   }
 
-  if ('.' in keyboard.keysDown){
+  if (',' in keyboard.keysDown){
     this.velocity.z = -this.speed
   }
 
