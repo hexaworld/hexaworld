af82f094404064ee06a81c8f571205cde5cdf118
freeman <the.freeman.lab@gmail.com>
2016-01-18 16:23:17 -0500

    put message in hexagon

--- ui/message.js
+++ ui/message.js
@@ -1,14 +1,49 @@
 var css = require('dom-css')
+var _ = require('lodash')
 
 module.exports = function (container) {
   var width = container.clientWidth
   var height = container.clientHeight
   var ismobile = width < height
 
+  var size = ismobile ? width * 0.9 : 0.65 * height
+
+  var points1 = _.range(7).map(function (i) {
+    var dx = (size / Math.sqrt(3)) * Math.cos(i * 2 * Math.PI / 6 + Math.PI / 6) + 1.1 * size / 2
+    var dy = (size / Math.sqrt(3)) * Math.sin(i * 2 * Math.PI / 6 + Math.PI / 6) + 1.1 * size / Math.sqrt(3)
+    return [dx, dy]
+  })
+
+  var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  svg.setAttribute('width', size * 1.1)
+  svg.setAttribute('height', size * (2 / Math.sqrt(3)) * 1.1)
+  css(svg, {
+    pointerEvents: 'none',
+    position: 'absolute',
+    left: 0, right: 0,
+    margin: '0px auto',
+    marginTop: ismobile 
+      ? (height - size) * 0.6 - 0.05 * size * (2 / Math.sqrt(3))
+      : (height - size) * 0.5 - 0.05 * size * (2 / Math.sqrt(3)),
+    display: 'block'
+  })
+  container.appendChild(svg)
+
+  var background = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
+  background.setAttribute('points', points1.join(' '))
+  css(background, {
+    fill: 'rgb(45,45,45)',
+    stroke: 'none',
+    zIndex: 2000
+  })
+  svg.appendChild(background)
+
   var label = document.createElement('div')
   container.appendChild(label)
   css(label, {
-    top: height * 0.35,
+    top: ismobile 
+      ? (height - size) * 0.6 - 0.05 * size * (2 / Math.sqrt(3)) + height * 0.2
+      : (height - size) * 0.5 - 0.05 * size * (2 / Math.sqrt(3)) + height * 0.25,
     margin: '0px auto',
     left: 0, right: 0,
     width: ismobile ? width : width * 0.6,
@@ -20,7 +55,7 @@ module.exports = function (container) {
   var message = document.createElement('div')
   label.appendChild(message)
   css(message, {
-    color: 'rgb(150,150,150)',
+    color: 'rgb(220,220,220)',
     fontFamily: 'Hack',
     fontSize: Math.sqrt(width * 5),
     opacity: 0
@@ -28,11 +63,13 @@ module.exports = function (container) {
   
   function show (text) {
     css(message, {opacity: 1})
+    css(background, {opacity: 1})
     message.innerHTML = text
   }
 
   function hide () {
     css(message, {opacity: 0})
+    css(background, {opacity: 0})
   }
 
   return {
